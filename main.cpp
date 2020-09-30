6e237e510673fb34cbd6a7d3e7b3169b375430c6
christeluwgb <christel@uwgb.edu>
2020-09-30 18:33:31 +0000

    final update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 
 class ListItem {
+  
   private:
+
     const char* item;
-    ListItem* next = NULL;
+    ListItem* next = nullptr;
     
   public:
     
     ListItem(const char *newItem) {
       item = newItem;
-      next = NULL;
+      next = nullptr;
     }
 
     ListItem* nextItem() {
       return next;
+    }
+
+    void setNextItem(ListItem* new_next) {
+      next = new_next;
     }
 
     const char* view() {
@@ -25,15 +31,35 @@ class ListItem {
 class LinkedList {
 
   private:
+
     ListItem* first_item;
     int list_length;
   
+    void deleteItem(ListItem* parent, ListItem* item) {
+      parent->setNextItem(item->nextItem());
+      delete item;
+    }
+
   public:
 
     LinkedList(const char *newItem) {
       first_item = new ListItem(newItem);
       list_length = 1;
     }
+
+    void addListItem(const char *newItem) {
+      ListItem* new_item = new ListItem(newItem);
+      ListItem* current_item = first_item;
+      ListItem* next_item = first_item->nextItem();
+      while (next_item != nullptr) {
+        current_item=next_item;
+        next_item = current_item->nextItem();
+      }
+      current_item->setNextItem(new_item);
+      list_length += 1;
+    }
+
+  
 
     int length() {
       return list_length;
@@ -46,15 +72,19 @@ class LinkedList {
     void outputList() {
       std::cout << first_item->view() << std::endl;
       ListItem* next_item = first_item->nextItem();
-      while (next_item != NULL) {
+      while (next_item != nullptr) {
         std::cout << next_item->view() << std::endl;
         next_item = next_item->nextItem();
       }
     }
+
 };
 
 int main() {
   LinkedList l("sam");
+  l.outputList();
+  l.outputLength();
+  l.addListItem("bob");
   l.outputList();
   l.outputLength();
 }
